841f6a73abdf8fa0c66963d16e2b5d80c86e46bc
mukhtarbdirahman <mukhtarabdirahmanm@gmail.com>
2021-05-18 06:50:11 +0300

    reverse sentence in place

--- app.js
+++ app.js
@@ -12,3 +12,27 @@ solution
 var myArr = [10, 5, 6, 9];
 var revMyArr = [].concat(myArr).reverse();
 console.log(revMyArr);
+
+
+
+2. Reverse Sentence In-place
+Write a function reverseSentence(A) that takes in an array of characters, A, and reverses the the "words" (not individual characters).
+
+Example:
+
+A = ['t','h','i','s',' ','i','s',' ','g','o','o','d'] reverseSentence(A) A // ['g','o','o','d',' ','i','s',' ','t','h','i','s']
+Share the Github link to your solution.
+
+
+
+solution
+const sentence = ['t','h','i','s',' ','i','s',' ','g','o','o','d'];
+
+function reverseWords(sentence) {
+    return sentence
+        .split(/(\s+)/)
+        .map(word => /^\s+$/.test(word) ? word : word.split('').reverse().join(''))
+        .join('');
+}
+
+console.log(reverseWords(sentence));
